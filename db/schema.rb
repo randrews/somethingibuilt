9c504542b31550770061b39b008c68403f2b3017
Ross Andrews <ross.andrews@gmail.com>
2015-09-09 15:59:54 -0500

    Projects scaffold, Bootstrap, jQuery

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150908012404) do
+ActiveRecord::Schema.define(version: 20150909200044) do
+
+  create_table "projects", force: true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.string   "category"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username",                            null: false
---
+++ spec/factories/projects.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :project do
+    name "MyString"
+    user_id 1
+    category "MyString"
+  end
+end
