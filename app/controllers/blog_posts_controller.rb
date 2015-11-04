124ae4c9538aab20882f692fdde9d53410f1ef76
Ross Andrews <ross.andrews@gmail.com>
2015-11-03 23:50:59 -0600

    Blog post previews and display page

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -3,10 +3,15 @@
 class BlogPostsController < ApplicationController
   before_action :authenticate_user!, :except => [:show, :index]
   before_action :set_blog_post, only: [:show, :update, :destroy, :edit]
-  before_action :set_project, only: [:edit]
-  before_filter :my_project, only: [:create, :edit, :update, :destroy]
+  before_action :set_project, only: [:edit, :show]
+  before_filter :my_project_and_post, only: [:edit, :update, :destroy]
+  before_filter :my_project, only: [:create]
 
   respond_to :html, :json
+
+  def show
+    @markdown_parser = markdown_parser(@project)
+  end
 
   def create
     @project = Project.find(params[:project_id])
@@ -27,7 +32,7 @@ class BlogPostsController < ApplicationController
       flash[:notice] = "Post updated"
       redirect_to project_url(params[:project_id])
     else
-      flash[:alert] = "Couldn't add this blog post: #{@blog_post.errors.full_messages.to_sentence}"
+      flash[:alert] = "Couldn't update this blog post: #{@blog_post.errors.full_messages.to_sentence}"
       render 'edit'
     end
   end
@@ -40,12 +45,21 @@ class BlogPostsController < ApplicationController
 
   private
 
-  def my_project
+  def my_project_and_post
     p = Project.find(params[:project_id])
     b = BlogPost.find(params[:id])
 
     if p.user_id != current_user.id || b.project != p
       flash[:alert] = "You are not the owner of this blog post"
+      redirect_to project_url(p)
+    end
+  end
+
+  def my_project
+    p = Project.find(params[:project_id])
+
+    if p.user_id != current_user.id
+      flash[:alert] = "You are not the owner of this project"
       redirect_to project_url(p)
     end
   end
--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -3,4 +3,20 @@ class BlogPost < ActiveRecord::Base
 
   validates :project_id, presence: true
   validates :text, presence: true
+
+  before_save :clean_newlines
+
+  def preview_text
+    return @preview_text if @preview_text
+
+    if text =~ /(.*?)\n/
+      @preview_text = $1
+    else
+      @preview_text = text
+    end
+  end
+
+  def clean_newlines
+    self.text = text.gsub("\r\n","\n")
+  end
 end
